b00079791753c956c9c6e6779175672c5a1de189
Alex Fedotov <alxfed@gmail.com>
2026-05-08 08:48:41 -0500

    initial release

--- src/machina_legens/cli.py
+++ src/machina_legens/cli.py
@@ -18,8 +18,8 @@ from .utilities import new_plato_text
 def options_and_arguments():
     # Initialize the parser
     parser = argparse.ArgumentParser(
-        description="Name-of-the-Machine thinks for you about the meanings.",
-        epilog="Example:  name-of-the-machine input_text.txt > output_text.txt"
+        description="Machina-Legens thinks for you about the meanings.",
+        epilog="Example:  machina-legens input_text.txt > output_text.txt"
         # thinking-machine -a multilogue.txt > tmp && mv tmp multilogue.txt
     )
 
@@ -54,10 +54,10 @@ def options_and_arguments():
 
 def run():
     """
-    $ text | name-of-the-machine                        # Accepts text from the pipe
-    $ echo "...<text>..." | name-of-the-machine         #
+    $ text | machina-legens                        # Accepts text from the pipe
+    $ echo "...<text>..." | machina-legens         #
 
-    $ name-of-the-machine multilogue.txt new_turn.txt    # ...or files.
+    $ machina-legens multilogue.txt new_turn.txt    # ...or files.
     """
 
     args = options_and_arguments().parse_args()
@@ -130,12 +130,12 @@ def run():
         utterance = "My answer is ready"
         # Open syslog connection
         syslog.openlog(
-            ident="name-of-the-machine",
+            ident="machina-legens",
             logoption=syslog.LOG_NDELAY,
             facility=syslog.LOG_USER
         )
         # Signal (single line less than 4096 only!)
-        syslog.syslog(syslog.LOG_INFO, f"name-of-the-machine: {utterance}.")
+        syslog.syslog(syslog.LOG_INFO, f"machina-legens: {utterance}.")
         syslog.closelog()
 
     except Exception as e:
